1ef9de5e890b8f21ffa9d15d97e9057d7bea1c80
fazaton <fazaton@gmail.com>
2017-06-12 11:50:07 +0000

    gosh

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
-  
+  resources :lessons, only: [:show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create] do
+        
+      end
+    end
   end
 end
