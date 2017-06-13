6845df92265e405a5c30d94eecd16c3dcf91dbb8
fazaton <fazaton@gmail.com>
2017-06-13 10:02:48 +0000

    gosh

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_enrolled_for_current_lesson, only: [:show]
+    
     def show
     end
     
@@ -7,5 +10,12 @@ class LessonsController < ApplicationController
     helper_method :current_lesson
     def current_lesson
        @current_lesson ||= Lesson.find(params[:id])
+    end
+    
+    
+    def require_enrolled_for_current_lesson
+        if not current_user.enrolled_in?(@current_user)
+            redirect_to courses_path, alert: 'You are not enrolled into the course!'
+        end
     end
 end
